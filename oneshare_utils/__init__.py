0b8619140318244ecffa9660dc74fc18b88d754c
masami10 <masami.nagasawa10@hotmail.com>
2021-09-04 11:24:41 +0800

    [ADD](utils)增加了标准工具集模块，增加了百度云AI对接
    Former-commit-id: 4ee5d6551e190b7e7746e97de7bcbcc5799bce76

---
+++ oneshare_utils/__init__.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+
+from . import baidu
+
+
+class CloudProvider(object):
+    def __init__(self, subclass=None, *args, **kwargs):
+        self._subclass = subclass
+        self._app = {}
+        if subclass:
+            self._app = self._subclass(*args, **kwargs)
+
+    def address_recognition(self, text):
+        raise NotImplementedError()
+
+    def open(self):
+        method = getattr(self._app, 'open', None)
+        if not method:
+            return False
+        return method()
+
+    def recognition(self, type, *args, **kargs):
+        return self._app.recognition(type, *args, **kargs)
