cc0dfe83fd2abe58a8df95d919d6c5fc181ce6ad
masami10 <masami.nagasawa10@hotmail.com>
2021-09-05 10:19:16 +0800

    [ADD](oneshare_odoo_modify)将登陆接口获取logo接口优化 2. 封装了标准的http返回信息:oneshare_json_success_resp,oneshare_json_fail_response

--- oneshare_utils/__init__.py
+++ oneshare_utils/__init__.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from . import baidu
+from . import http
 
 
 class CloudProvider(object):
---
+++ oneshare_utils/http.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+
+from http import HTTPStatus
+from odoo.http import Response
+import logging
+import json
+
+_logger = logging.getLogger(__name__)
+
+MAGIC_ERROR_CODE = 59999  # 永远不会定义
+
+ONESHARE_HTTP_ERROR_CODE = {
+    40001: "Tightening Tool Is Not Success Configuration"
+}
+
+
+def oneshare_json_success_resp(status_code=HTTPStatus.OK, **kwargs):
+    headers = [('Content-Type', 'application/json')]
+    if status_code == HTTPStatus.NO_CONTENT:
+        resp = Response(status=status_code, headers=headers)
+        return resp
+    if status_code > HTTPStatus.BAD_REQUEST:
+        _logger.error("Success Response The Status Code Must Be Less Than 400, But Now Is {0}".format(status_code))
+        return Response(HTTPStatus.OK, headers=headers)
+    data = {
+        "status_code": status_code,
+        "msg": kwargs.get("msg") or kwargs.get("message"),
+    }
+    extra = kwargs.get("extra", "") or kwargs.get("extra_info", "")
+    if extra:
+        data.update({
+            "extra": extra
+        })
+    body = json.dumps(data)
+    headers.append(('Content-Length', len(body)))
+    resp = Response(body, status=status_code, headers=headers)
+    return resp
+
+
+def oneshare_json_fail_response(error_code=MAGIC_ERROR_CODE, status_code=HTTPStatus.BAD_REQUEST, **kwargs):
+    msg = ONESHARE_HTTP_ERROR_CODE.get(error_code)
+    if not msg:
+        _logger.error("Error Code: {0} Is Not Defined".format(error_code))
+        msg = kwargs.get("msg") or kwargs.get("message"),
+    data = {
+        "status_code": status_code,
+        "msg": msg,
+        "extra": kwargs.get("extra", "") or kwargs.get("extra_info", "")
+    }
+    body = json.dumps(data)
+    headers = [('Content-Type', 'application/json'), ('Content-Length', len(body))]
+    return Response(body, status=status_code, headers=headers)
