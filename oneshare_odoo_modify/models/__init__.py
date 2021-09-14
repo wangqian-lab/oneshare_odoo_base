cb21e01919be462cf2a954427b02ead8f2f6208a
masami10 <masami.nagasawa10@hotmail.com>
2021-09-14 16:30:50 +0800

    [ADD](modify)1.增加了category mixin抽象类 2. resource模型默认时区修改
    Former-commit-id: 0c2c8d95f983c1e646d5edd891ac846103d438c5

--- oneshare_odoo_modify/models/__init__.py
+++ oneshare_odoo_modify/models/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 from . import ir_http
 from . import res_partner
+from . import oneshare_category_mixin
+from . import resource
---
+++ oneshare_odoo_modify/models/oneshare_category_mixin.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from random import randint
+
+
+class OneshareCategoryMixin(models.AbstractModel):
+    _name = "oneshare.category.mixin"
+    _description = "Category Mixin"
+
+    def _get_default_color(self):
+        return randint(1, 11)
+
+    name = fields.Char(string="Tag Name", required=True)
+    color = fields.Integer(string='Color Index', default=_get_default_color)
