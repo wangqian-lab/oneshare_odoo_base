226bdc5ef6d4fd7568a5cfd8d25a28f4d6ecd666
masami10 <masami.nagasawa10@hotmail.com>
2021-09-17 16:06:09 +0800

    [ADD](oidc)增加了环境变量ENV_ONESHARE_SIGNUP_PUBLIC_USER控制是否创建可登陆系统的用户
    Former-commit-id: de2c7115d8f27a21095a452370f712b2c7f6f949

--- auth_oidc/models/res_users.py
+++ auth_oidc/models/res_users.py
@@ -1,12 +1,45 @@
 import requests
 
-from odoo import api, models
+from odoo import api, models, _
+from odoo.tools import ustr
 from odoo.exceptions import AccessDenied
+from odoo.addons.auth_signup.models.res_users import SignupError
 from odoo.http import request
+from ast import literal_eval
+from distutils.util import strtobool
+import os
+
+ENV_ONESHARE_SIGNUP_PUBLIC_USER = strtobool(os.getenv('ENV_ONESHARE_SIGNUP_PUBLIC_USER', 'False'))
 
 
 class ResUsers(models.Model):
     _inherit = "res.users"
+
+    @api.model
+    def _signup_create_user(self, values):
+        if ENV_ONESHARE_SIGNUP_PUBLIC_USER:
+            return self._create_user_from_public_user(values)
+        return super(ResUsers, self)._signup_create_user(values)
+
+    def _create_user_from_public_user(self, values):
+        template_user_id = literal_eval(self.env['ir.config_parameter'].sudo().get_param('base.public_user', 'False'))
+        template_user = self.browse(template_user_id)
+        if not template_user.exists():
+            raise ValueError(_('Signup: invalid public user'))
+
+        if not values.get('login'):
+            raise ValueError(_('Signup: no login given for new user'))
+        if not values.get('partner_id') and not values.get('name'):
+            raise ValueError(_('Signup: no name or partner given for new user'))
+
+        # create a copy of the template user (attached to a specific partner_id if given)
+        values['active'] = True
+        try:
+            with self.env.cr.savepoint():
+                return template_user.with_context(no_reset_password=True).copy(values)
+        except Exception as e:
+            # copy may failed if asked login is not available.
+            raise SignupError(ustr(e))
 
     @staticmethod
     def _auth_oauth_get_tokens_implicit_flow(oauth_provider, params):
