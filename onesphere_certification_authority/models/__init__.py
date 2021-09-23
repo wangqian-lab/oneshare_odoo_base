41bc19575fa833897497797f816d6a9364d086b5
masami10 <masami.nagasawa10@hotmail.com>
2021-09-23 16:10:56 +0800

    [ADD](ca)onesphere_certification_authority模块增加

---
+++ onesphere_certification_authority/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import certification_authority
+from . import res_config_settings
---
+++ onesphere_certification_authority/tests/common.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo.tests import common
+
+
+class TestOneshareCACommon(common.TransactionCase):
+
+    def setUp(self):
+        super(TestOneshareCACommon, self).setUp()
