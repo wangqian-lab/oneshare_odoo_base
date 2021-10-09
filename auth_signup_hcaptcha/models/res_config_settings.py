be817d4224cb38d6190220d80f761c42e7b9cd6a
masami10 <masami.nagasawa10@hotmail.com>
2021-10-09 13:27:53 +0800

    [ADD](login)add hcaptcha integration

---
+++ auth_signup_hcaptcha/models/res_config_settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import _, api, fields, models
+import os
+
+ENV_DEFAULT_LANGUAGE = os.getenv('ENV_DEFAULT_LANGUAGE', 'zh_CN')
+
+
+@api.model
+def _lang_get(self):
+    return self.env['res.lang'].get_installed()
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    module_auth_signup_hcaptcha = fields.Boolean("hCAPTCHA: Easy on Humans, Hard on Bots")
+
+    hcaptcha_public_key = fields.Char("Site Key", config_parameter='hcaptcha_public_key', groups='base.group_system')
+    hcaptcha_private_key = fields.Char("Secret Key", config_parameter='hcaptcha_private_key',
+                                       groups='base.group_system')
+    hcaptcha_lang = fields.Selection(_lang_get, string='Hcaptcha Load Language', config_parameter='hcaptcha_lang',
+                                     default=ENV_DEFAULT_LANGUAGE)
--- odoo_web_login/controllers/main.py
+++ odoo_web_login/controllers/main.py
@@ -26,6 +26,7 @@ from odoo.addons.web.controllers.main import Home
 import pytz
 import datetime
 import logging
+from distutils.util import strtobool
 
 import odoo
 import odoo.modules.registry
@@ -42,21 +43,20 @@ class LoginHome(Home):
     @http.route('/web/login', type='http', auth="none")
     def web_login(self, redirect=None, **kw):
         param_obj = request.env['ir.config_parameter'].sudo()
-        request.params['disable_footer'] = ast.literal_eval(param_obj.get_param('login_form_disable_footer')) or False
-        request.params['disable_database_manager'] = ast.literal_eval(
-            param_obj.get_param('login_form_disable_database_manager')) or False
+        request.params['disable_footer'] = param_obj.get_param('login_form_disable_footer') or False
+        request.params['disable_database_manager'] = param_obj.get_param('login_form_disable_database_manager') or False
 
-        change_background = ast.literal_eval(param_obj.get_param('login_form_change_background_by_hour')) or False
+        change_background = param_obj.get_param('login_form_change_background_by_hour') or False
         if change_background:
             config_login_timezone = param_obj.get_param('login_form_change_background_timezone')
             tz = config_login_timezone and pytz.timezone(config_login_timezone) or pytz.utc
             current_hour = datetime.datetime.now(tz=tz).hour or 10
 
-            if (current_hour >= 0 and current_hour < 3) or (current_hour >= 18 and current_hour < 24):  # Night
+            if (0 <= current_hour < 3) or (18 <= current_hour < 24):  # Night
                 request.params['background_src'] = param_obj.get_param('login_form_background_night') or ''
-            elif current_hour >= 3 and current_hour < 7:  # Dawn
+            elif 3 <= current_hour < 7:  # Dawn
                 request.params['background_src'] = param_obj.get_param('login_form_background_dawn') or ''
-            elif current_hour >= 7 and current_hour < 16:  # Day
+            elif 7 <= current_hour < 16:  # Day
                 request.params['background_src'] = param_obj.get_param('login_form_background_day') or ''
             else:  # Dusk
                 request.params['background_src'] = param_obj.get_param('login_form_background_dusk') or ''
