7ec38d6217bf3ba79e67fd8f75b072c687b37071
masami10 <masami.nagasawa10@hotmail.com>
2021-12-10 21:13:20 +0800

    [ADD](web_echarts)增加了echarts模块
    Former-commit-id: 6b653043a2f5f9761cde2137322dadd987cee996

---
+++ web_echarts/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "web_echarts",
+
+    'summary': """
+        echarts插件""",
+
+    'description': """
+        echarts插件
+    """,
+
+    'author': "上海文享数据科技有限公司",
+    'website': "http://www.oneshare.com.cn",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Hidden/Tools',
+    'version': '14.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['web'],
+
+    # always loaded
+    'data': [
+        'views/web_widget_echart.xml',
+    ],
+}
