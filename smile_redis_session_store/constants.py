a9d0b45be9a37080c1c03910cd2b7e509a2d21e3
masami10 <masami.nagasawa10@hotmail.com>
2022-09-14 15:05:38 +0800

    [IMP]更新了一些环境变量

--- smile_redis_session_store/constants.py
+++ smile_redis_session_store/constants.py
@@ -6,12 +6,12 @@ from odoo import tools
 
 SESSION_TIMEOUT = 60 * 60 * 24 * 7  # 1 weeks in seconds
 
-redis_hosts: str = tools.config.get('redis_host', None) or os.getenv('ENV_SESSION_REDIS_HOST', 'localhost')
+redis_hosts: str = tools.config.get('redis_host', None) or os.getenv('ENV_REDIS_HOST', 'localhost')
 startup_nodes = redis_hosts.split(',')
 
-redis_ports = int(tools.config.get('redis_port') or os.getenv('ENV_SESSION_REDIS_PORT', '6379'))
+redis_ports = int(tools.config.get('redis_port') or os.getenv('ENV_REDIS_PORT', '6379'))
 redis_db = int(tools.config.get('redis_dbindex') or os.getenv('ENV_SESSION_REDIS_DB', '1'))
-redis_password = tools.config.get('redis_pass') or os.getenv('ENV_SESSION_REDIS_PASSWORD', None)
+redis_password = tools.config.get('redis_pass') or os.getenv('ENV_REDIS_PASSWORD', None)
 
 
 def is_redis_session_store_activated():
